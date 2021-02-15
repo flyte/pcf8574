45383546b2029c3ca306a0417e41a40479d15f3c
Ellis Percival <flyte@failcode.co.uk>
2021-02-15 23:33:49 +0000

    Add type hints. Remove unnecessary list inheritance.

--- pcf8574/__init__.py
+++ pcf8574/__init__.py
@@ -1,62 +1,75 @@
-import smbus
+from typing import Any, Iterable, List
+
+import smbus  # type: ignore
 
 
-class IOPort(list):
+class IOPort:
     """
     Represents the PCF8574 IO port as a list of boolean values.
     """
-    def __init__(self, pcf8574, *args, **kwargs):
-        super(IOPort, self).__init__(*args, **kwargs)
+
+    def __init__(self, pcf8574):
+        # type: (PCF8574) -> None
         self.pcf8574 = pcf8574
 
     def __setitem__(self, key, value):
+        # type: (int, bool) -> None
         """
         Set an individual output pin.
         """
         self.pcf8574.set_output(key, value)
 
     def __getitem__(self, key):
+        # type: (int) -> bool
         """
         Get an individual pin state.
         """
         return self.pcf8574.get_pin_state(key)
 
     def __repr__(self):
+        # type: () -> str
         """
         Represent port as a list of booleans.
         """
         state = self.pcf8574.bus.read_byte(self.pcf8574.address)
         ret = []
         for i in range(8):
-            ret.append(bool(state & 1<<7-i))
+            ret.append(bool(state & 1 << 7 - i))
         return repr(ret)
 
     def __len__(self):
+        # type: () -> int
         return 8
 
     def __iter__(self):
+        # type: () -> Iterable[bool]
         for i in range(8):
             yield self[i]
 
     def __reversed__(self):
+        # type: () -> Iterable[bool]
         for i in range(8):
-            yield self[7-i]
+            yield self[7 - i]
 
 
-class PCF8574(object):
+class PCF8574:
     """
     A software representation of a single PCF8574 IO expander chip.
     """
+
     def __init__(self, i2c_bus_no, address):
+        # type: (int, int) -> None
         self.bus_no = i2c_bus_no
         self.bus = smbus.SMBus(i2c_bus_no)
         self.address = address
 
     def __repr__(self):
+        # type: () -> str
         return "PCF8574(i2c_bus_no=%r, address=0x%02x)" % (self.bus_no, self.address)
 
     @property
     def port(self):
+        # type: () -> IOPort
         """
         Represent IO port as a list of boolean values.
         """
@@ -64,31 +77,35 @@ class PCF8574(object):
 
     @port.setter
     def port(self, value):
+        # type: (List[bool]) -> None
         """
         Set the whole port using a list.
         """
-        assert isinstance(value, list)
         assert len(value) == 8
         new_state = 0
         for i, val in enumerate(value):
             if val:
-                new_state |= 1 << 7-i
+                new_state |= 1 << 7 - i
         self.bus.write_byte(self.address, new_state)
 
     def set_output(self, output_number, value):
+        # type: (int, bool) -> None
         """
         Set a specific output high (True) or low (False).
         """
-        assert output_number in range(8), "Output number must be an integer between 0 and 7"
+        assert output_number in range(
+            8
+        ), "Output number must be an integer between 0 and 7"
         current_state = self.bus.read_byte(self.address)
-        bit = 1 << 7-output_number
-        new_state = current_state | bit if value else current_state & (~bit & 0xff)
+        bit = 1 << 7 - output_number
+        new_state = current_state | bit if value else current_state & (~bit & 0xFF)
         self.bus.write_byte(self.address, new_state)
 
     def get_pin_state(self, pin_number):
+        # type: (int) -> bool
         """
         Get the boolean state of an individual pin.
         """
         assert pin_number in range(8), "Pin number must be an integer between 0 and 7"
         state = self.bus.read_byte(self.address)
-        return bool(state & 1<<7-pin_number)
+        return bool(state & 1 << 7 - pin_number)
